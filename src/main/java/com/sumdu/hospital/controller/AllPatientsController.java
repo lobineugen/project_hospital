f25750eb566e62a2287b4c2b16fa91ba7d0f223b
Lobin <lobin.eugene@gmail.com>
2019-05-03 00:20:22 +0300

    Implemented simple export to excel(98-2003)

--- src/main/java/com/sumdu/hospital/controller/AllPatientsController.java
+++ src/main/java/com/sumdu/hospital/controller/AllPatientsController.java
@@ -2,24 +2,36 @@ package com.sumdu.hospital.controller;
 
 import com.sumdu.hospital.database.DAO;
 import com.sumdu.hospital.model.Patient;
+import com.sumdu.hospital.service.Export;
+import com.sumdu.hospital.service.ShowDialog;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 @Component
 public class AllPatientsController {
     private static final Logger LOGGER = Logger.getLogger(AllPatientsController.class);
+    @FXML
+    private AnchorPane allPatientsPane;
     @FXML
     private TableView<Patient> allPatients;
     @FXML
@@ -30,9 +42,11 @@ public class AllPatientsController {
     private TableColumn<Patient, Integer> age;
     @FXML
     private TextField searchValue;
+    @FXML
+    private Button exportToXSLButton;
     private DAO dao;
     private ObservableList<Patient> patientObservableList = FXCollections.observableArrayList();
-
+    private ApplicationContext context;
 
     @Autowired
     public AllPatientsController(DAO dao) {
@@ -66,5 +80,26 @@ public class AllPatientsController {
             }
         });
 
+        exportToXSLButton.addEventFilter(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                FileChooser fileChooser = new FileChooser();
+                FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Microsoft Office Excel (97-2003)", "*.xls");
+                fileChooser.getExtensionFilters().add(extFilter);
+                File file = fileChooser.showSaveDialog(allPatientsPane.getScene().getWindow());
+                Export export = context.getBean(Export.class);
+                export.export(dao, file);
+                ShowDialog showDialog = context.getBean(ShowDialog.class);
+                showDialog.showInformationDialog("Дані успішно експортовані в Excel");
+            }
+        });
+
+
     }
+
+    @Autowired
+    public void context(ApplicationContext context) {
+        this.context = context;
+    }
+
 }
