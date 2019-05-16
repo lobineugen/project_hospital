a0f8dcc6aaa331c527c268e876c0c2cd3c8d91f3
lobineugene <lobin.eugene@gmail.com>
2019-05-16 16:12:15 +0300

    Merge branch 'master' of C:\SVN\project_hospital with conflicts.

--- src/main/java/com/sumdu/hospital/service/ShowDialog.java
+++ src/main/java/com/sumdu/hospital/service/ShowDialog.java
@@ -1,20 +1,16 @@
 package com.sumdu.hospital.service;
 
 import com.jfoenix.controls.JFXDatePicker;
-import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
 import com.sumdu.hospital.model.Card;
-import com.sumdu.hospital.model.ExpertConsultation;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
-import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.util.Callback;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,8 +20,6 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.sql.Date;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 import static com.sumdu.hospital.constants.Constants.*;
@@ -34,7 +28,11 @@ import static com.sumdu.hospital.constants.Constants.*;
 public class ShowDialog {
     private Helper helper;
     private ApplicationContext context;
-
+    private Button createButton;
+    private JFXTextField number;
+    private JFXTextField week;
+    private JFXDatePicker dateIn;
+    private JFXDatePicker dateOut;
 
     public void showInformationDialog(String information, Pane primaryStage) {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
@@ -64,11 +62,6 @@ public class ShowDialog {
     }
 
     public Optional<Card> createCard(Pane primaryStage) {
-        Button createButton;
-        JFXTextField number = new JFXTextField();
-        JFXTextField week = new JFXTextField();
-        JFXDatePicker dateIn = new JFXDatePicker();
-        JFXDatePicker dateOut = new JFXDatePicker();
         helper = context.getBean(Helper.class);
         Dialog<Card> dialog = new Dialog<>();
         dialog.setTitle("Вікно створення");
@@ -79,6 +72,10 @@ public class ShowDialog {
         imageView.setFitHeight(50);
         imageView.setFitWidth(50);
         dialog.setGraphic(imageView);
+        number = new JFXTextField();
+        week = new JFXTextField();
+        dateIn = new JFXDatePicker();
+        dateOut = new JFXDatePicker();
         dateIn.setConverter(getStringConverter());
         dateOut.setConverter(getStringConverter());
 
@@ -98,6 +95,20 @@ public class ShowDialog {
         createButton = (Button) dialog.getDialogPane().lookupButton(createButtonType);
         createButton.setDisable(true);
 
+        number.textProperty().addListener(getStringListener(number));
+        week.textProperty().addListener(getStringListener(week));
+        dateIn.valueProperty().addListener(getDateListener(dateIn));
+        dateOut.valueProperty().addListener(getDateListener(dateOut));
+
+        dialog.setResultConverter(new Callback<ButtonType, Card>() {
+            @Override
+            public Card call(ButtonType param) {
+                if (param.equals(createButtonType)) {
+                    return new Card(helper.getUniqueID(), number.getText(), Date.valueOf(dateIn.getValue()), Date.valueOf(dateOut.getValue()), week.getText());
+                }
+                return null;
+            }
+        });
         GridPane grid = new GridPane();
         grid.setHgap(10);
         grid.setVgap(30);
@@ -112,21 +123,6 @@ public class ShowDialog {
         grid.add(new Label("Дата госпіталізації:"), 0, 3);
         grid.add(dateIn, 1, 3);
 
-        number.textProperty().addListener(getStringListener(number, grid.getChildren(), createButton, 4));
-        week.textProperty().addListener(getStringListener(week, grid.getChildren(), createButton, 4));
-        dateIn.valueProperty().addListener(getDateListener(dateIn, grid.getChildren(), createButton, 4));
-        dateOut.valueProperty().addListener(getDateListener(dateOut, grid.getChildren(), createButton, 4));
-
-        dialog.setResultConverter(new Callback<ButtonType, Card>() {
-            @Override
-            public Card call(ButtonType param) {
-                if (param == createButtonType) {
-                    return new Card(helper.getUniqueID(), number.getText(), Date.valueOf(dateIn.getValue()), Date.valueOf(dateOut.getValue()), week.getText());
-                }
-                return null;
-            }
-        });
-
         dialog.getDialogPane().setContent(grid);
         return dialog.showAndWait();
     }
@@ -136,58 +132,42 @@ public class ShowDialog {
         this.context = context;
     }
 
-    private ChangeListener<String> getStringListener(TextInputControl textField, List<Node> list, Button button, int count) {
+    private ChangeListener<String> getStringListener(JFXTextField textField) {
         return new ChangeListener<String>() {
             @Override
             public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-                int cnt = check(list);
-                if (cnt == count) {
-                    button.setDisable(false);
+                if (!number.getText().isEmpty()
+                        && !week.getText().isEmpty()
+                        && dateIn.getValue() != null
+                        && dateOut.getValue() != null) {
+                    createButton.setDisable(false);
                 } else {
-                    button.setDisable(true);
+                    createButton.setDisable(true);
                 }
-                if (textField instanceof JFXTextArea) {
-                    ((JFXTextArea) textField).validate();
-                } else if (textField instanceof JFXTextField) {
-                    ((JFXTextField) textField).validate();
-                }
+                textField.validate();
             }
         };
     }
 
-    private ChangeListener<LocalDate> getDateListener(JFXDatePicker datePicker, List<Node> list, Button button, int count) {
+    private ChangeListener<LocalDate> getDateListener(JFXDatePicker datePicker) {
         return new ChangeListener<LocalDate>() {
             @Override
             public void changed(ObservableValue<? extends LocalDate> observable, LocalDate oldValue, LocalDate newValue) {
-                int cnt = check(list);
-                if (cnt == count) {
-                    button.setDisable(false);
+                if (!number.getText().isEmpty()
+                        && !week.getText().isEmpty()
+                        && dateIn.getValue() != null
+                        && dateOut.getValue() != null) {
+                    createButton.setDisable(false);
                 } else {
-                    button.setDisable(true);
+                    createButton.setDisable(true);
                 }
                 datePicker.validate();
             }
         };
     }
 
-
-    private int check(List<Node> list) {
-        int result = 0;
-        for (Node node : list) {
-            if (node instanceof JFXTextField && !((JFXTextField) node).getText().isEmpty()) {
-                result++;
-            } else if (node instanceof JFXDatePicker && ((JFXDatePicker) node).getValue() != null) {
-                result++;
-            } else if (node instanceof JFXTextArea && !((JFXTextArea) node).getText().isEmpty()) {
-                result++;
-            }
-        }
-        return result;
-    }
-
-    public Optional<ExpertConsultation> createExpertConsultation(Pane primaryStage, ExpertConsultation expertConsultation) throws IOException {
-        helper = context.getBean(Helper.class);
-        Dialog<ExpertConsultation> dialog = new Dialog<>();
+    public Optional createExpertConsultation(Pane primaryStage) throws IOException {
+        Dialog<Card> dialog = new Dialog<>();
         dialog.setTitle("Вікно створення");
         dialog.setHeaderText("Заповніть всі поля щоб створити новий запис");
         dialog.initOwner(primaryStage.getScene().getWindow());
@@ -196,60 +176,10 @@ public class ShowDialog {
         imageView.setFitHeight(50);
         imageView.setFitWidth(50);
         dialog.setGraphic(imageView);
+
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(
                 "/fxml/expertConsultationsListCell.fxml"));
         Pane pane = fxmlLoader.load();
-        ButtonType buttonType;
-        if (expertConsultation == null) {
-            buttonType = new ButtonType("Створити", ButtonBar.ButtonData.OK_DONE);
-        } else {
-            buttonType = new ButtonType("Оновити", ButtonBar.ButtonData.OK_DONE);
-        }
-
-        dialog.getDialogPane().getButtonTypes().addAll(buttonType, ButtonType.CANCEL);
-        Button createButton = (Button) dialog.getDialogPane().lookupButton(buttonType);
-        createButton.setDisable(true);
-
-        JFXDatePicker date = (JFXDatePicker) ((HBox) pane.getChildren().get(0)).getChildren().get(0);
-        JFXTextField doctor = (JFXTextField) ((HBox) pane.getChildren().get(0)).getChildren().get(1);
-        JFXTextArea conclusion = (JFXTextArea) pane.getChildren().get(2);
-        date.setConverter(getStringConverter());
-
-        if (expertConsultation != null) {
-            if (expertConsultation.getDate() != null) {
-                date.setValue(expertConsultation.getDate().toLocalDate());
-            }
-            doctor.setText(expertConsultation.getDoctor());
-            conclusion.setText(expertConsultation.getConclusion());
-        }
-
-        helper.addRequiredValidator(date);
-        helper.addRequiredValidator(doctor);
-        helper.addRequiredValidator(conclusion);
-
-        List<Node> list = new ArrayList<>();
-        list.add(date);
-        list.add(doctor);
-        list.add(conclusion);
-
-        doctor.textProperty().addListener(getStringListener(doctor, list, createButton, 3));
-        conclusion.textProperty().addListener(getStringListener(conclusion, list, createButton, 3));
-        date.valueProperty().addListener(getDateListener(date, list, createButton, 3));
-
-        dialog.setResultConverter(new Callback<ButtonType, ExpertConsultation>() {
-            @Override
-            public ExpertConsultation call(ButtonType param) {
-                if (param == buttonType && expertConsultation == null) {
-                    return new ExpertConsultation(helper.getUniqueID(), Date.valueOf(date.getValue()), doctor.getText(), conclusion.getText());
-                } else if (param == buttonType) {
-                    expertConsultation.setDate(Date.valueOf(date.getValue()));
-                    expertConsultation.setDoctor(doctor.getText());
-                    expertConsultation.setConclusion(conclusion.getText());
-                    return expertConsultation;
-                }
-                return null;
-            }
-        });
 
         dialog.getDialogPane().setContent(pane);
         return dialog.showAndWait();
